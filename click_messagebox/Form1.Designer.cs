45fda5ec29361e5dcc028275116bad83b750b40e
HMJda <hmj4669@gmail.com>
2022-11-25 23:14:28 +0900

    사이즈 조절

--- click_messagebox/Form1.Designer.cs
+++ click_messagebox/Form1.Designer.cs
@@ -34,7 +34,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(356, 133);
             this.Name = "Form1";
             this.Text = "MessageBoxButtonsApp";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
