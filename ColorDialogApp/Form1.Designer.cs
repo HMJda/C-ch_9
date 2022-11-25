6a18fb0085aa78072c76836d9cff5b116a96563d
HMJda <hmj4669@gmail.com>
2022-11-25 23:15:11 +0900

    버튼 사이즈 조절

--- ColorDialogApp/Form1.Designer.cs
+++ ColorDialogApp/Form1.Designer.cs
@@ -43,7 +43,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(44, 44);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(126, 23);
+            this.button1.Size = new System.Drawing.Size(161, 46);
             this.button1.TabIndex = 0;
             this.button1.Text = "폼 색상 변경";
             this.button1.UseVisualStyleBackColor = true;
@@ -51,9 +51,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(44, 129);
+            this.button2.Location = new System.Drawing.Point(44, 128);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(126, 23);
+            this.button2.Size = new System.Drawing.Size(161, 43);
             this.button2.TabIndex = 1;
             this.button2.Text = "버튼 색상 변경";
             this.button2.UseVisualStyleBackColor = true;
@@ -63,7 +63,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(209, 193);
+            this.ClientSize = new System.Drawing.Size(250, 211);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
